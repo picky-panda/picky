fbda4af801d1400c36bd4d531968445c113c8d97
Seoa-S <0325sandy@ewhain.net>
2024-02-15 12:54:45 +0900

    login: 구현

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -58,7 +59,6 @@ dependencies {
     implementation("com.google.android.gms:play-services-location:17.0.0")
 
     //google login
-    implementation(platform("com.google.firebase:firebase-bom:32.7.2")) // Import the Firebase BoM
-    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
 
 }
--- app/src/main/java/org/gdsc_android/picky_panda/LoginActivity.kt
+++ app/src/main/java/org/gdsc_android/picky_panda/LoginActivity.kt
@@ -1,11 +1,63 @@
 package org.gdsc_android.picky_panda
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import org.gdsc_android.picky_panda.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private val RequestCode_SIGN_IN = 123 //임의의 값
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        //googleSignInOptions 설정
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("444216138764-3ai31eo0j37jpbs8if4srvklulnr2bq1.apps.googleusercontent.com")
+            .build()
+        
+        //googleSignInClient. 로그인 창 표시
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
+         
+        binding.googleSignInButton.setOnClickListener{
+            val signInIntent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RequestCode_SIGN_IN)
+         }
     }
+    
+    override  fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
+        super.onActivityResult(requestCode, resultCode, data)
+        
+        //google 로그인 결과 처리
+        if (requestCode == RequestCode_SIGN_IN){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: com.google.android.gms.tasks.Task<GoogleSignInAccount>) {
+        try{
+            //google 로그인 성공 시
+            val account = completedTask.getResult(ApiException::class.java)
+
+            //account를 사용해 로그인 또는 회원가입 로직 수행
+            //여기에 로그인 또는 회원가입 성공 시의 작업을 추가
+
+            //로그인 성공 후 메인 화면으로 이동
+            startActivity(Intent(this, HomeFragment::class.java))
+            finish()
+        } catch (e: ApiException){
+            //google 로그인 실패했을 때 작업 추가
+        }
+    }
+
+
 }
