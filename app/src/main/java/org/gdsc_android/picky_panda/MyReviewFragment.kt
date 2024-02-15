0748167896d477a4f69c4e70f301aba81c72e305
Minhye Kim <144666264+minhye1013@users.noreply.github.com>
2024-02-16 07:42:02 +0900

    Fragment 간 화면 전환 기능 initial 구현

--- app/src/main/java/org/gdsc_android/picky_panda/MyReviewFragment.kt
+++ app/src/main/java/org/gdsc_android/picky_panda/MyReviewFragment.kt
@@ -5,25 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import org.gdsc_android.picky_panda.SettingFragment
-import org.gdsc_android.picky_panda.databinding.FragmentSettingBinding
+import org.gdsc_android.picky_panda.MyReviewFragment
+import org.gdsc_android.picky_panda.databinding.FragmentMyReviewBinding
 
-class SettingFragment : Fragment() {
-    private lateinit var binding: FragmentSettingBinding
+class MyReviewFragment : Fragment() {
 
+    private lateinit var binding: FragmentMyReviewBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentSettingBinding.inflate(inflater, container, false)
+        binding = FragmentMyReviewBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // SettingFragment에서 ImageButton 클릭 시 MypageFragment로 돌아감
-        binding.fromSettingToMyPageButton.setOnClickListener {
+        // SavedFragment에서 ImageButton 클릭 시 MypageFragment로 돌아감
+        binding.fromMyReviewToMyPageButton.setOnClickListener {
             (activity as MainActivity).replaceFragment(MyPageFragment())
         }
     }
