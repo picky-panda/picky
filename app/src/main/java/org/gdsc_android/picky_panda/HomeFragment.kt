14287844f243106312fd58beeaf63fdb2a957c8d
Seoa-S <0325sandy@ewhain.net>
2024-02-16 15:34:47 +0900

    home view: xml 수정

--- app/src/main/java/org/gdsc_android/picky_panda/HomeFragment.kt
+++ app/src/main/java/org/gdsc_android/picky_panda/HomeFragment.kt
@@ -12,12 +12,8 @@ import org.gdsc_android.picky_panda.databinding.FragmentHomeBinding
 class HomeFragment : Fragment(), OnMapReadyCallback {
 
     private var _binding: FragmentHomeBinding? = null
-    //실제로 사용할 수 있는 프로퍼티.
-    //_binding의 null 여부를 확인하고 non-null인 경우에만 해당 바인딩 정보를 반환
     private val binding get() = _binding!!
 
-   //fragment가 최초로 화면에 표시될 때 호출되는 메서드
-    //_binding 변수 초기화 및 해당 fragment의 xml 레이아웃을 인플레이트하여 view 생성 후 반환
     override fun onCreateView(
        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
    ): View? {
@@ -26,9 +22,8 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
    }
 
     //fragment의 view가 소멸되는 시점에 호출
-    //이때 _binding을 null로 설정시켜 메모리 누수를 방지한다 fragment의 view가 다시 생성될 때 _binding은 다시 초기화 된다
     override fun onDestroyView() {
-        super.onDestroyView() //부모 클래스인 Fragment 클래스의 onDestroyViewㄹ르 호출. 소멸 전 필요한 작업 수행.
+        super.onDestroyView()
         _binding = null
     }
 
