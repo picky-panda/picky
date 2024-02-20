14b4c314293f861f1b71e6a6c774bd2a60c269e4
Seoa-S <0325sandy@ewhain.net>
2024-02-20 17:43:07 +0900

    serviceCreator

--- app/src/main/java/org/gdsc_android/picky_panda/data/ServiceApi.kt
+++ app/src/main/java/org/gdsc_android/picky_panda/data/ServiceApi.kt
@@ -1,4 +1,17 @@
 package org.gdsc_android.picky_panda.data
 
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
 interface ServiceApi {
+
+    @Headers("Content-Type: application/json")
+    @POST("/restaurant/save/:restaurantId") //가게 저장
+    fun saveStore(@Body request: RequestSaveStoreData)
+        : Call<RequestSaveStoreData>
+
+
+
 }
